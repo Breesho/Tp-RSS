07a3600bf75982b450b9d1f4fa1d1965c9635118
SabrinaMonclair <sabrina.monclair@hotmail.com>
2020-07-03 11:38:29 +0200

    404

---
+++ pages/404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>erreur 404</title>
+</head>
+<body>
+    <div class="error">
+        <div class="row justify-content-center">
+            <div class="col-sm-6 mt-3 mb-3">
+                <div class="h2 text-center text-uppercase">Deso !</div>
+                <div class="text-center"><img src="" alt="erreur404"></div>
+                <div class="h5 text-center">Veuillez revenir sur la page d'accueil afin de continuer votre navigation.</div>
+                <div class='text-center'><a class="h3 text-uppercase" href="index.php">We have to go back !</a></div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
