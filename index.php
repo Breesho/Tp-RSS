eae8b21c333255630ecd6fb3cc83f3ee5d3fb331
yvesmaried <yvesmarie.drouard@gmail.com>
2020-07-03 13:53:39 +0200

    remove x modal

--- index.php
+++ index.php
@@ -202,9 +202,6 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="col-12 modal-title text-center">date</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
         </div>
         <div>
           <p class="col-12 modal-title text-center">titre</p>
